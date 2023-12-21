4b05769fb373a13bc496e9f72ab39e649e596bbc
Lawshika <154485467+Lawshika@users.noreply.github.com>
2023-12-21 19:23:29 +0530

    Add files via upload

---
+++ Firefox.java
@@ -0,0 +1,19 @@
+package week1.day1;
+
+public class Firefox {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+float browserVersion = 100.2f;
+String browserName = "fiefox";
+boolean isVisible = true ;
+int releaseYear =1998 ;
+char browserLogo = 'f';
+System.out.println("BrowserVersion :"+browserVersion);
+System.out.println("BrowserName :"+browserName);
+System.out.println("IsVisible ?"+isVisible);
+System.out.println("ReleaseYear :"+releaseYear);
+System.out.println("BrowserLogo :"+browserLogo);
+	}
+
+}
